9c092488b71ffb2b1ad3da8a5d8cfe6a26125188
Leticia Oliveira <92490205+Let011@users.noreply.github.com>
2022-04-10 16:50:40 -0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,8 @@
+salario = float(input('Informe o salario: '))
+desconto  = ( 7 / 100)
+reajuste = ( 5 / 100) 
+salario = (salario + reajuste - desconto)
+
+print('O salario a receber é: ', salario)
+
+
